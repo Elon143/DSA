6138451b16b5765e4908a6ba62c60e8402c68daf
Shivam <shivamkumar0a12@gamil.com>
2022-09-10 19:18:55 +0100

    AVL Tree Rotation done

---
+++ 82_AVL__Tree_Rotation.c
@@ -0,0 +1,131 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct Node
+{
+    int key; // value of node 
+    struct Node *left;
+    struct Node *right;
+    int height;
+};
+
+int getHeight(struct Node *n){
+    if(n==NULL)
+        return 0;
+    return n->height; // niche wala commented code bhi same work karega
+    // else{
+    //     return n->height;
+    // }
+}
+
+struct Node *createNode(int key)
+{
+    struct Node* node = (struct Node *) malloc(sizeof(struct Node));
+    node->key = key;
+    node->left = NULL;
+    node->right = NULL;
+    node->height = 1;
+    return node;
+};
+// func __max
+int max(int a, int b){
+    return a>b?a:b;  // if a>b return - a else return b // this is turnary operator
+}
+
+// This func will return balance factor of tree
+int getBalanceFactor(struct Node *n){
+    if(n==NULL){
+        return 0;
+    }
+    // return n->left->height - n->right->height;  // next line works same as this 
+    return getHeight(n->left) - getHeight(n->right);
+}
+
+// RL rotationn
+struct Node* rightRotate(struct Node* y){
+    struct Node* x = y->left;
+    struct Node* T2 = x->right;
+
+    x->right = y;
+    x->left = T2;
+
+    y->height = max(getHeight(y->right),getHeight(y->left)) + 1;
+    x->height = max(getHeight(x->right),getHeight(x->left)) + 1;
+
+    return x;
+}
+
+// LR rotation
+struct Node* leftRotate(struct Node* x){
+    struct Node* y = x->right;
+    struct Node* T2 = y->left;
+
+    y->left = x;
+    x->right = T2;
+
+    y->height = max(getHeight(y->right),getHeight(y->left)) + 1;
+    x->height = max(getHeight(x->right),getHeight(x->left)) + 1;
+
+    return y;
+}
+
+struct Node *insert(struct Node* node, int key){
+    if(node == NULL)
+        return (createNode(key));
+
+    if (key < node->key)
+        node->left = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+        return node;
+
+    node ->height = 1+ max(getHeight(node->left),getHeight(node->right));
+    int bf = getBalanceFactor(node);
+
+    // Left Left Case  
+    if(bf>1 && key< node->left->key){
+        rightRotate(node);
+    }  
+    // Right Right Case    
+    if(bf<-1 && key> node->right->key){
+        return leftRotate(node);
+    }  
+    // Left Right Case
+    if (bf>1 && key > node->left->key)
+    {
+        node->left = leftRotate(node->left);
+        return rightRotate(node);
+    }
+    // Right Left Case    
+    if (bf<-1 && key < node->left->key)
+    {
+        node->left = rightRotate(node->right);
+        return leftRotate(node);
+    }
+        
+}
+
+void preOrder(struct Node *root)
+{
+    if (root != NULL)
+    {
+        printf("%d ", root->key);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+
+int main(){
+    struct Node * root = NULL; // This is a root node
+    root = insert(root,1);
+    root = insert(root,2);
+    root = insert(root,4);
+    root = insert(root,5);
+    root = insert(root,6);
+    root = insert(root,3);
+
+    preOrder(root); // ans = 4 2 1 3 5 6
+
+    return 0;
+}
